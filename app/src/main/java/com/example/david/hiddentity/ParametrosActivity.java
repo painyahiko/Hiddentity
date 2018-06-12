7a767f5f176f46a38ea5e6d9bfe3171f6a0e081d
xxYrYxx <xkillrayx@gmail.com>
2018-06-13 00:38:01 +0200

    se añadio activity parametros

---
+++ app/src/main/java/com/example/david/hiddentity/ParametrosActivity.java
@@ -0,0 +1,13 @@
+package com.example.david.hiddentity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParametrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametros);
+    }
+}
