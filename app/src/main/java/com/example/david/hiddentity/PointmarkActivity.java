55555ea182eb78c2df23297365f1a2c8f5ccb8cf
painyahiko <david.valiente.martinez@gmail.com>
2018-06-14 19:25:17 +0200

    estilos

--- app/src/main/java/com/example/david/hiddentity/PointmarkActivity.java
+++ app/src/main/java/com/example/david/hiddentity/PointmarkActivity.java
@@ -32,6 +32,17 @@ public class PointmarkActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pointmark);
 
         rojoInfo = findViewById(R.id.rojoInfo);
@@ -55,8 +66,8 @@ public class PointmarkActivity extends AppCompatActivity {
         }
 
         listView.setAdapter(datos);
-        rojoInfo.setText("El equipo rojo tiene " + partida.redPoints + " puntos");
-        azulInfo.setText("El equipo azul tiene " + partida.bluePoints + " puntos");
+        rojoInfo.setText(partida.redPoints + "");
+        azulInfo.setText(partida.bluePoints + "");
 
 
         startGame.setOnClickListener(new View.OnClickListener() {
